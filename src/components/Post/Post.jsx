05cab8c5fd97ce386fe140f7daf537f7ff127c30
Vander Lima <vnd.vander@gmail.com>
2022-07-16 15:53:26 -0300

    [feat:] Structure Form and Styling Form

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -35,6 +35,23 @@ export function Post() {
           <a href="#">#ouroiv</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder='Deixe um comentário'
+        />
+
+        <footer>
+          <button
+            type='submit'
+          >
+            Publicar
+          </button>
+        </footer>
+
+      </form>
     </article >
   )
 }
