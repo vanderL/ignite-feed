090ff250191e94511edde890a700f3f030c59463
Vander Lima <vnd.vander@gmail.com>
2022-07-19 16:34:16 -0300

    [refactor:] Component Avatar

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,10 +1,15 @@
 import { Trash, ThumbsUp } from 'phosphor-react'
+import { Avatar } from '../Avatar/Avatar'
 import styles from './Comment.module.css'
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/diego3g.png" alt="" />
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/diego3g.png"
+        alt=""
+      />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
