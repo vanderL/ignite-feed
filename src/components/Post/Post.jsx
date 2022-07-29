3afafd59b46c972e5194c5a7df65ac17efbc87b1
Vander Lima <vnd.vander@gmail.com>
2022-07-28 23:20:34 -0300

    [fix:] Add Unique Key

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -55,10 +55,10 @@ export function Post({ author, content, publishedAt }) {
       <div className={styles.content}>
         {content.map(line => {
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>
           } else if (line.type === 'link') {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="#">{line.content}</a>
               </p>
             )
@@ -90,7 +90,7 @@ export function Post({ author, content, publishedAt }) {
       </form>
 
       <div className={styles.commentList}>
-        {comments.map(comment => (
+        {comments?.map(comment => (
           <Comment
             key={comment}
             content={comment}
