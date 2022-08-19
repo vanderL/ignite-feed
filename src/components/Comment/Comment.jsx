146771ae1be80878e5157db9b3bbe42b01a77b07
Vander Lima <vnd.vander@gmail.com>
2022-08-18 23:47:55 -0300

    [feat:]Aplly like in comments

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,11 +1,18 @@
 import { Trash, ThumbsUp } from 'phosphor-react'
+import { useState } from 'react'
 import { Avatar } from '../Avatar/Avatar'
 import styles from './Comment.module.css'
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0)
+
   function handleDeleteComment() {
 
     onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1)
   }
 
   return (
@@ -42,10 +49,10 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp
             />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
